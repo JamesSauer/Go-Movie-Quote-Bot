72d43fd202074557872b78bc2ceefab9d7381cf0
James Sauer <mail@jamessauer.com>
2019-07-01 00:56:42 +0200

    getRandomMoviePage() now works.

--- movie_quote_bot.go
+++ movie_quote_bot.go
@@ -1,24 +1,92 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"golang.org/x/net/html"
 	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
 )
 
-// Returns a random movie title.
-func getRandomMovie() (movieTitle string) {
-	// TODO: Where to look for movies? themoviedb.org API?
-	// TODO: Implement function.
-	movieTitle = "Forrest Gump"
+var listParts = [8]string{
+	// Note how these are not regular dashes ("-"), but longer ones ("–").
+	"A–C",
+	"D–F",
+	"G–I",
+	"J–L",
+	"M–O",
+	"P–S",
+	"T–V",
+	"W–Z",
+}
+
+var findRedlink = regexp.MustCompile("redlink=1")
+
+func extractLink(n *html.Node) (string, error) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				if findRedlink.MatchString(a.Val) {
+					return "", errors.New("Found link was a red link")
+				}
+				return a.Val, nil
+			}
+		}
+	}
+	if n.FirstChild == nil {
+		return "", errors.New("No link found")
+	}
+	return extractLink(n.FirstChild)
+}
+
+func fetchPage(URL string) (page string) {
+	res, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	page = string(body)
 	return
 }
 
-// Takes a movie title, searches WikiQuotes for it and returns its entry.
-// If no entry can be found, returns an error instead.
-func getMoviePage(movieTitle string) (moviePage string, err error) {
-	// TODO: Implement function.
-	fileContent, err := ioutil.ReadFile("test_page.html")
-	moviePage = string(fileContent)
+// Returns a random WikiQuotes page about a movie.
+func getRandomMoviePage() (moviePage string) {
+	rand.Seed(time.Now().UnixNano())
+	startURL := fmt.Sprintf("https://en.wikiquote.org/wiki/List_of_films_(%s)", listParts[rand.Intn(8)])
+
+	listPage := fetchPage(startURL)
+	doc, err := html.Parse(strings.NewReader(listPage))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	movieLinks := make([]string, 0)
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "i" {
+			link, err := extractLink(n)
+			if err == nil {
+				movieLinks = append(movieLinks, link)
+			}
+			return
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+
+	randomLink := fmt.Sprintf("%s", movieLinks[rand.Intn(len(movieLinks))])
+	moviePage = fetchPage("https://en.wikiquote.org" + randomLink)
 	return
 }
 
@@ -33,4 +101,5 @@ func getQuotes(moviePage string) (quotes map[string][]string) {
 func main() {
 	// TODO: Select random quote.
 	fmt.Println(`My momma always said, "Life was like a box of chocolates. You never know what you're gonna get."`)
+	ioutil.WriteFile("test.html", []byte(getRandomMoviePage()), 0600)
 }
